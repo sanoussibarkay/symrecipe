df0c98ed57d8b0b5ad1aa0dcf768e243ed5ad6e4
sanoussibarkay <barkaysanoussi@yahoo.fr>
2026-08-08 11:19:08 +0100

    Add homepage

---
+++ src/Controller/HomController.php
@@ -0,0 +1,20 @@
+<?php 
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class HomController  extends AbstractController
+{
+#[Route('/', name: 'home.index', methods: ['GET'])]
+    public function index(): Response
+    {
+        return $this->render('home.html.twig', [
+            'controller_name' => 'HomController',
+        ]);
+      
+    }
+}
+
+?>
